824df1aec6bf54624b545f983de32b7bb4bf7a95
LEEBEEE <101805395+LEEBEEE@users.noreply.github.com>
2022-07-15 14:21:58 +0900

    BootStrap

---
+++ bootstrap/image.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>bootstrap</title>
+</head>
+<body>
+    <div class="container">
+        <img src="https://images.pexels.com/photos/9271052/pexels-photo-9271052.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=1280&w=1920" class="img-fluid">
+        <img src="https://images.pexels.com/photos/9271052/pexels-photo-9271052.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=1280&w=1920" class="img-thumbnail">
+        <img src="https://frontendeval.com/images/puppy-3.jpeg" class="rounded">
+        <div>
+            <img src="https://frontendeval.com/images/puppy-3.jpeg" class="rounded mx-auto d-block">
+        </div>
+        <div class="d-flex justify-content-center">
+            <img src="https://frontendeval.com/images/puppy-3.jpeg" class="rounded">
+        </div>
+        <div class="text-center">
+            <img src="https://frontendeval.com/images/puppy-3.jpeg" class="rounded">
+        </div>
+    </div>
+</body>
+</html>
---
+++ bootstrap/picture.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>picture</title>
+</head>
+<body>
+    <picture>
+        <source media="(min-width: 700px)" srcset="https://i.picsum.photos/id/482/900/500.jpg?hmac=f0GS21Uf3BnrYD8SySfymB7apsFc-cspXJuKJwHD6Ow">
+        <source media="(min-width: 400px)" srcset="https://i.picsum.photos/id/212/400/240.jpg?hmac=mGhlieQPADP6fEKU9BN3zoAcUZL38wENHWpv9KinktA">
+        <img src="https://i.picsum.photos/id/639/200/300.jpg?hmac=dITw9zyqi0A4tZ6lMk191HJezQPJDDKG4wyJXadYRH0" alt="ferris wheel">
+    </picture>
+</body>
+</html>
